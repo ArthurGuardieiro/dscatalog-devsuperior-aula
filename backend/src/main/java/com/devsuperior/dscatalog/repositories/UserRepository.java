741708baf3efa489f1bcb4f5cd79e08a4befcf2c
Arthur Guardieiro <arthur.aaguardieiro@icloud.com>
2023-07-23 20:13:06 -0300

    application-dev, busca personalizada com nome do produto

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dscatalog.entities.User;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	User findByEmail(String email);
+
 }
 
 
